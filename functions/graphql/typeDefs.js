14133c61da6df95077b12f507db48fa70003418a
Ilfa <ilfa@insvire.com>
2020-12-15 10:29:42 +0700

    Firebase & GraphQl Prepare

---
+++ functions/graphql/typeDefs.js
@@ -0,0 +1,16 @@
+const gql = require('graphql-tag')
+
+module.exports = gql `
+type Post {
+    id: ID!
+    owner: String!
+    text: String!
+    createdAt: String!
+    likeCount: Int!
+    commentCount: Int!
+}
+
+type Query {
+    getPosts: [Post]!
+}
+`
